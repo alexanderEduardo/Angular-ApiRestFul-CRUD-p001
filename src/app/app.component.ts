7b686ae6342823e0271292a28bd1e497aea05ace
Alexander <alexdev0411@gmail.com>
2022-06-08 18:48:26 -0500

    My first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-init-proyect';
+export class AppComponent { //Se pone export para que se puede importar desde app.module.ts
+  title = 'Bienvenido a Angular';
+  curso:string = 'Curso Spring con Angular :)';
+  user:string = 'Alexander Eduardo';
+ /*  getName():void{
+    console.log("Hello World");
+  } */
 }
