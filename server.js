d243de9b8a1be4a748d7edbd505d1fb988fdaabe
Alexander <alexdev0411@gmail.com>
2022-06-15 16:36:24 -0500

    actulizacion de server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-springboot-restful'));
+app.use(express.static('./dist/angular-springboot-restful'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-springboot-restful/index.html'));
+res.sendFile('index.html',{root: 'dist/angular-springboot-restful'});
 });
 
 // Start the app by listening on the default Heroku port
